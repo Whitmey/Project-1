20575afee2eaf735c8b034f97b8368134debd83f
Will Whitmey <w.whitmey@outlook.com>
2016-10-10 14:34:55 +0100

    working 5

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 // These actions happen when a player clicks on a horse
 var horseChosen;
 var betAmount;
-var balance = 1000;
+var balance = 100;
 document.getElementById("balance").innerHTML = balance;
 
 
@@ -59,16 +59,16 @@ var random = function() {
 
 var winner;
 var findWinner = function() {
-  if (randomNumber < 0.45) {
+  if (randomNumber < 0.35) {
     winner = "horse1";
   }
-  else if (randomNumber > 0.45 && randomNumber < 0.6) {
+  else if (randomNumber > 0.35 && randomNumber < 0.5) {
     winner = "horse2";
   }
-  else if (randomNumber > 0.6 && randomNumber < 0.77) {
+  else if (randomNumber > 0.5 && randomNumber < 0.7) {
     winner = "horse3";
   }
-  else if (randomNumber > 0.77 && randomNumber < 0.9) {
+  else if (randomNumber > 0.7 && randomNumber < 0.9) {
     winner = "horse4";
   }
   else {
@@ -81,37 +81,37 @@ var result = function() {
   if (winner === horseChosen) {
       if (horseChosen === "horse1") {
         var oddsN = document.getElementById("odds1").innerHTML;
-        document.getElementById("result").innerHTML = "Your horse came first and you won" + (betAmount * oddsN);
+        document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;//add the bet amount to the balance.
       }
       else if (horseChosen === "horse2") {
         var oddsN = document.getElementById("odds2").innerHTML;
-        document.getElementById("result").innerHTML = "Your horse came first and you won" + (betAmount * oddsN);
+        document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;//add the bet amount to the balance.
       }
       else if (horseChosen === "horse3") {
         var oddsN = document.getElementById("odds3").innerHTML;
-        document.getElementById("result").innerHTML = "Your horse came first and you won" + (betAmount * oddsN);
+        document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;
       }
       else if (horseChosen === "horse4") {
         var oddsN = document.getElementById("odds4").innerHTML;
-        document.getElementById("result").innerHTML = "Your horse came first and you won" + (betAmount * oddsN);
+        document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;
       }
       else if (horseChosen === "horse5") {
         var oddsN = document.getElementById("odds5").innerHTML;
-        document.getElementById("result").innerHTML = "Your horse came first and you won" + (betAmount * oddsN);
+        document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;
     }
   }
   else if (winner !== horseChosen) {
-    document.getElementById("result").innerHTML = "Your horse came last and you lost" + betAmount;
+    document.getElementById("result").innerHTML = "You Lost! £" + betAmount;
     balance = parseInt(balance) - parseInt(betAmount);
     console.log(balance);
     document.getElementById("balance").innerHTML = balance;//subtract the bet amount from the balance.
@@ -120,12 +120,13 @@ var result = function() {
 
 var assignOdds = function() {
   document.getElementById("odds1").innerHTML = 2;
-  document.getElementById("odds2").innerHTML = 4;
-  document.getElementById("odds3").innerHTML = 6;
-  document.getElementById("odds4").innerHTML = 8;
-  document.getElementById("odds5").innerHTML = 10;
+  document.getElementById("odds2").innerHTML = 3;
+  document.getElementById("odds3").innerHTML = 5;
+  document.getElementById("odds4").innerHTML = 7;
+  document.getElementById("odds5").innerHTML = 9;
 };
 assignOdds();
+
 
 // write a program or adapt the find winner function so that the odds change according to how high the number is.
 // or rewrite the chosen winner program to incorporate the odds variation.
