1b26fb027aeea27b60df76299f5769d54312fa9d
Will Whitmey <w.whitmey@outlook.com>
2016-10-11 13:09:18 +0100

    progress with animations

--- js/app.js
+++ js/app.js
@@ -9,79 +9,86 @@ document.getElementById("balance").innerHTML = balance;
 
 document.getElementById("horse1").addEventListener('click', function() {
   horseChosen = "horse1";
-  betAmount = document.getElementById("bet").value; // Get the amount of money bet and assign it the variable
+  betAmount = document.getElementById("bet").value; // Get the amount of money bet and assign it to the variable
   $("#balance").fadeOut(0);
-  random(); // Create a random number
-  findWinner(); // Randomly pick a winner
-  result(); // Decide whether the user chose the correct horse.
 });
 document.getElementById("horse2").addEventListener('click', function() {
   horseChosen = "horse2";
   betAmount = document.getElementById("bet").value;
   $("#balance").fadeOut(0);
-  random();
-  findWinner();
-  result();
 });
 document.getElementById("horse3").addEventListener('click', function() {
   horseChosen = "horse3";
   betAmount = document.getElementById("bet").value;
   $("#balance").fadeOut(0);
-  random();
-  findWinner();
-  result();
 });
 document.getElementById("horse4").addEventListener('click', function() {
   horseChosen = "horse4";
   betAmount = document.getElementById("bet").value;
   $("#balance").fadeOut(0);
-  random();
-  findWinner();
-  result();
 });
 document.getElementById("horse5").addEventListener('click', function() {
   horseChosen = "horse5";
   betAmount = document.getElementById("bet").value;
   $("#balance").fadeOut(0);
-  random();
-  findWinner();
-  result();
 });
 
-document.getElementById("nextRace").addEventListener("click", function() {
-  assignOdds();
+document.getElementById("race").addEventListener("click", function() {
+  myMove();
+  checkForWinner();
 });
 document.getElementById("reset").addEventListener("click", function() {
   balance = 100;
   document.getElementById("balance").innerHTML = balance;
 });
 
+var assignOdds = function() {
+  document.getElementById("odds1").innerHTML = 2;
+  document.getElementById("odds2").innerHTML = 3;
+  document.getElementById("odds3").innerHTML = 5;
+  document.getElementById("odds4").innerHTML = 7;
+  document.getElementById("odds5").innerHTML = 10;
+};
+assignOdds();
 
-var randomNumber;
-var random = function() {
-  randomNumber = Math.random();
-}
 
+// $(document).ready(function(){
+//   $("#fadeIn").click(function(){
+//     myMove();
+//     $("#result").fadeIn(3000);
+//     $("#balance").fadeIn(2500);
+//     $("#result").fadeOut(2000);
+//
+//     console.log("clicked");
+//   });
+// });
 
-var winner;
-var findWinner = function() {
-  if (randomNumber < 0.45) {
-    winner = "horse1";
-  }
-  else if (randomNumber > 0.45 && randomNumber < 0.6) {
-    winner = "horse2";
-  }
-  else if (randomNumber > 0.6 && randomNumber < 0.8) {
-    winner = "horse3";
-  }
-  else if (randomNumber > 0.8 && randomNumber < 0.9) {
-    winner = "horse4";
-  }
-  else {
-    winner = "horse5";
+function myMove() {
+
+  var id = setInterval(frame, 10);
+
+  function frame() {
+    for(var i=0;i<horses.length;i++) {
+      var pos = parseFloat(horses[i].style.left || 0) + Math.round(parseFloat(horses[i].dataset.odds) + (Math.random()*10));
+      horses[i].style.left = pos + "px";
+      if (pos >= 1350) {
+        clearInterval(id);
+        checkForWinner();
+      }
+    }
   }
 };
 
+
+function checkForWinner() {
+  var winner = horses[0];
+
+  for(var i=0;i<horses.length;i++) {
+    var finalPos = parseFloat(horses[i].style.left);
+    if(finalPos > parseFloat(winner.style.left)) winner = horses[i];
+  }
+  console.log(winner.textContent, Array.prototype.indexOf.call(horses, winner));
+}
 
 var result = function() {
   if (winner === horseChosen) {
@@ -129,55 +136,3 @@ var result = function() {
     document.getElementById("result").style.color = "red";
   }
 };
-
-var assignOdds = function() {
-  document.getElementById("odds1").innerHTML = 2;
-  document.getElementById("odds2").innerHTML = 3;
-  document.getElementById("odds3").innerHTML = 5;
-  document.getElementById("odds4").innerHTML = 7;
-  document.getElementById("odds5").innerHTML = 10;
-};
-assignOdds();
-
-
-// write a program or adapt the find winner function so that the odds change according to how high the number is.
-// or rewrite the chosen winner program to incorporate the odds variation.
-
-$(document).ready(function(){
-  $("#fadeIn").click(function(){
-    myMove();
-    $("#result").fadeIn(3000);
-    $("#balance").fadeIn(2500);
-    $("#result").fadeOut(2000);
-
-    console.log("clicked");
-  });
-});
-
-function myMove() {
-
-  var id = setInterval(frame, 10);
-
-  function frame() {
-    for(var i=0;i<horses.length;i++) {
-      var pos = parseFloat(horses[i].style.left || 0) + Math.round(parseFloat(horses[i].dataset.odds) + (Math.random()*10));
-      horses[i].style.left = pos + "px";
-      if (pos >= 1350) {
-        clearInterval(id);
-        checkForWinner();
-      }
-    }
-  }
-};
-
-
-function checkForWinner() {
-  var winner = horses[0];
-
-  for(var i=0;i<horses.length;i++) {
-    var finalPos = parseFloat(horses[i].style.left);
-    if(finalPos > parseFloat(winner.style.left)) winner = horses[i];
-  }
-
-  console.log(winner.textContent, Array.prototype.indexOf.call(horses, winner));
-}
