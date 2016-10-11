12136f83243b471d077d737f0d4ffcfcb2e0181e
Will Whitmey <w.whitmey@outlook.com>
2016-10-11 11:04:59 +0100

    Slightly different layout with space for animation

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ document.getElementById("balance").innerHTML = balance;
 document.getElementById("horse1").addEventListener('click', function() {
   horseChosen = "horse1";
   betAmount = document.getElementById("bet").value; // Get the amount of money bet and assign it the variable
+  $("#balance").fadeOut(0);
   random(); // Create a random number
   findWinner(); // Randomly pick a winner
   result(); // Decide whether the user chose the correct horse.
@@ -16,6 +17,7 @@ document.getElementById("horse1").addEventListener('click', function() {
 document.getElementById("horse2").addEventListener('click', function() {
   horseChosen = "horse2";
   betAmount = document.getElementById("bet").value;
+  $("#balance").fadeOut(0);
   random();
   findWinner();
   result();
@@ -23,6 +25,7 @@ document.getElementById("horse2").addEventListener('click', function() {
 document.getElementById("horse3").addEventListener('click', function() {
   horseChosen = "horse3";
   betAmount = document.getElementById("bet").value;
+  $("#balance").fadeOut(0);
   random();
   findWinner();
   result();
@@ -30,6 +33,7 @@ document.getElementById("horse3").addEventListener('click', function() {
 document.getElementById("horse4").addEventListener('click', function() {
   horseChosen = "horse4";
   betAmount = document.getElementById("bet").value;
+  $("#balance").fadeOut(0);
   random();
   findWinner();
   result();
@@ -37,6 +41,7 @@ document.getElementById("horse4").addEventListener('click', function() {
 document.getElementById("horse5").addEventListener('click', function() {
   horseChosen = "horse5";
   betAmount = document.getElementById("bet").value;
+  $("#balance").fadeOut(0);
   random();
   findWinner();
   result();
@@ -46,7 +51,7 @@ document.getElementById("nextRace").addEventListener("click", function() {
   assignOdds();
 });
 document.getElementById("reset").addEventListener("click", function() {
-  balance = 1000;
+  balance = 100;
   document.getElementById("balance").innerHTML = balance;
 });
 
@@ -59,16 +64,16 @@ var random = function() {
 
 var winner;
 var findWinner = function() {
-  if (randomNumber < 0.35) {
+  if (randomNumber < 0.45) {
     winner = "horse1";
   }
-  else if (randomNumber > 0.35 && randomNumber < 0.5) {
+  else if (randomNumber > 0.45 && randomNumber < 0.6) {
     winner = "horse2";
   }
-  else if (randomNumber > 0.5 && randomNumber < 0.7) {
+  else if (randomNumber > 0.6 && randomNumber < 0.8) {
     winner = "horse3";
   }
-  else if (randomNumber > 0.7 && randomNumber < 0.9) {
+  else if (randomNumber > 0.8 && randomNumber < 0.9) {
     winner = "horse4";
   }
   else {
@@ -82,35 +87,37 @@ var result = function() {
       if (horseChosen === "horse1") {
         var oddsN = document.getElementById("odds1").innerHTML;
         document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
-        $( "button" ).click(function() {
-          $( "#result" ).show( "slow" );
-        });
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;//add the bet amount to the balance.
+        document.getElementById("result").style.color = "#99ff99";
       }
       else if (horseChosen === "horse2") {
         var oddsN = document.getElementById("odds2").innerHTML;
         document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;//add the bet amount to the balance.
+        document.getElementById("result").style.color = "#99ff99";
       }
       else if (horseChosen === "horse3") {
         var oddsN = document.getElementById("odds3").innerHTML;
         document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;
+        document.getElementById("result").style.color = "#99ff99";
       }
       else if (horseChosen === "horse4") {
         var oddsN = document.getElementById("odds4").innerHTML;
         document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;
+        document.getElementById("result").style.color = "#99ff99";
       }
       else if (horseChosen === "horse5") {
         var oddsN = document.getElementById("odds5").innerHTML;
         document.getElementById("result").innerHTML = "1st! You won £" + (betAmount * oddsN);
         balance = parseInt(balance) + parseInt(betAmount * oddsN);
         document.getElementById("balance").innerHTML = balance;
+        document.getElementById("result").style.color = "#99ff99";
     }
   }
   else if (winner !== horseChosen) {
@@ -118,6 +125,7 @@ var result = function() {
     balance = parseInt(balance) - parseInt(betAmount);
     console.log(balance);
     document.getElementById("balance").innerHTML = balance;//subtract the bet amount from the balance.
+    document.getElementById("result").style.color = "red";
   }
 };
 
@@ -126,7 +134,7 @@ var assignOdds = function() {
   document.getElementById("odds2").innerHTML = 3;
   document.getElementById("odds3").innerHTML = 5;
   document.getElementById("odds4").innerHTML = 7;
-  document.getElementById("odds5").innerHTML = 9;
+  document.getElementById("odds5").innerHTML = 10;
 };
 assignOdds();
 
@@ -138,7 +146,7 @@ $(document).ready(function(){
   $("#fadeIn").click(function(){
     $("#result").fadeIn(3000);
     $("#balance").fadeIn(2500);
-    $("#result").fadeOut(3000);
+    $("#result").fadeOut(2000);
 
     console.log("clicked");
   });
